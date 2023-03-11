5b1ba54d9712a502cd8d6a599055784c1175b744
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-11 00:27:02 +0000

    [Add]birthdayカラム,notnull制約

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_10_073413) do
+ActiveRecord::Schema.define(version: 2023_03_11_000847) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
