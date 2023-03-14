b4a0e4e7504912eba9ef9f9836f8a4e20ab46754
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-14 14:09:14 +0000

    [Fix]enum選択肢名

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   has_many :scores
 
   enum sex: { man: 0, woman: 1, another: 2 }
-  enum part: { lead: 0, top: 1, second: 2, third: 3, forth: 4, bass: 5, voicepercussion: 6  
+  enum part: { lead: 0, top: 1, chorus2: 2, chorus3: 3, chorus4: 4, bass: 5, voicepercussion: 6 }
 
-    def self.guest
+  def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
