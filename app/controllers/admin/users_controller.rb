4db95c4729b33878fa6b21698511f585cbee58f9
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-14 22:55:45 +0000

    [Create]usersコントローラ

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,27 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to users_mypage_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :nickname, :birthday, :part)
+  end
 end
