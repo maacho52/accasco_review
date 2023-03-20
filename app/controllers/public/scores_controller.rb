91c2ad33a5360d228b9d04532f5e5e3358d102fd
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-20 15:07:46 +0000

    [Create]楽譜投稿ユーザーコントローラ、viewファイル

--- app/controllers/public/scores_controller.rb
+++ app/controllers/public/scores_controller.rb
@@ -1,2 +1,28 @@
 class Public::ScoresController < ApplicationController
+  
+  def new
+    @user = current_user
+    @score = Score.new
+  end
+  
+  def index
+    @scores = all_scores.page(params[:page]).per(12)
+    #@user = current_user
+    #@user.scores = current_user.scores.all
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  private
+  
+  def score_params
+    params.require(:score).permit(:name, :artist, :member, :difficulty)
+  end
 end
