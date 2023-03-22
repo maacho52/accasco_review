78d14194f37e6dfaff04f6bc3fdfb5ad31cb5adc
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-22 12:23:59 +0000

    [Add]enum、enum日本語化

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,7 @@ class Score < ApplicationRecord
   has_many :arranges
   belongs_to :user
   belongs_to :site
+  
+  enum member: { three: 0, four: 1, five: 2, six: 3, seven_or_more: 4, boys: 5, girls: 6 }
+  enum difficulty: { introduction: 0, biginner: 1, intermediate: 2, advanced: 3, superlative: 4 }
 end
