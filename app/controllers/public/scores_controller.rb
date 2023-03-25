5e6014d10558a01e4e974dac2cc745e6c9efdd8b
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-25 00:35:06 +0000

    [Create]edit,show,createアクション [Add]indexアクション

--- app/controllers/public/scores_controller.rb
+++ app/controllers/public/scores_controller.rb
@@ -1,27 +1,41 @@
 class Public::ScoresController < ApplicationController
-  
+
   def new
     @user = current_user
     @score = Score.new
   end
-  
+
   def index
     @scores = all_scores.page(params[:page]).per(12)
+    @all_scores_count = all_scores.count
     #@user = current_user
     #@user.scores = current_user.scores.all
   end
-  
+
   def create
+    @score = Score.new(score_params)
+    @score.user_id = current_user.id
+    if @score.save
+      flash[:notice] = "楽譜を投稿しました"
+      redirect_to score_path(@score.id)
+    else
+      @scores = Score.all
+      render :index
+    end
+
   end
-  
+
   def edit
+    @score = Score.find(params[:id])
+    @user = current_user
   end
-  
+
   def show
+    @score = Score.find(params[:id])
   end
-  
+
   private
-  
+
   def score_params
     params.require(:score).permit(:name, :artist, :member, :difficulty)
   end
