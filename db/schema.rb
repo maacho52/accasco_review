b192a4c38de9a7828e6d3f542914b10ff06ed00a
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-28 08:27:01 +0000

    [Add]星レビュー機能カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_14_130739) do
+ActiveRecord::Schema.define(version: 2023_03_28_082505) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2023_03_14_130739) do
     t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "star", null: false
   end
 
   create_table "scores", force: :cascade do |t|
