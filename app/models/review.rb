225f1a99284b14084a575df44e9a821e9a058d06
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-28 10:32:28 +0000

    [Add]dependent: :destoroy,[Create] 星評価空禁止且つ1以上5未満

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,11 @@
 class Review < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :user
   belongs_to :score
+
+  # 星の評価の空を禁止する、且つ1以上、5未満
+  validates :star, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1,
+  }, presence: true
 end
