311315aad5fe223677f8c98920e764b014366939
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-28 10:34:14 +0000

    [Create]星評価機能

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,2 +1,13 @@
 class Public::ReviewsController < ApplicationController
+  
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to score_path(@review.score)
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:score_id, :title, :body, :star)
+    end
 end
