ddfc67bbe583b12e2da380ac48701f18744809b7
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-29 13:58:31 +0000

    [Add]createアクション　@review.user_id if文

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,9 +1,14 @@
 class Public::ReviewsController < ApplicationController
-  
+
   def create
     @review = Review.new(review_params)
-    @review.save
-    redirect_to score_path(@review.score)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to score_path(@review.score)
+    else
+      @review = Review.find(params[:id])
+      render "score/show"
+    end
   end
 
   private
