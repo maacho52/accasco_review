84dbfbc886a66c8b1c5edc723381bf56fb78a077
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-03-29 13:26:48 +0000

    [Add]星バリデーション

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
   belongs_to :score
+  validates :star, presence: true
 
   # 星の評価の空を禁止する、且つ1以上、5未満
   validates :star, numericality: {
