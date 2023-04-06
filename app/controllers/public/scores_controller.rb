07bed1e9a0f525abc0d6907a086d0d8cdb693746
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-06 12:54:49 +0000

    [Create]save_arrange アレンジタグを用いた楽譜の投稿用記述[Add]createアクション arrange_list

--- app/controllers/public/scores_controller.rb
+++ app/controllers/public/scores_controller.rb
@@ -15,7 +15,10 @@ class Public::ScoresController < ApplicationController
   def create
     @score = Score.new(score_params)
     @score.user_id = current_user.id
+     # 受け取った値を,で区切って配列にする
+    arrange_list = params[:score][:name].split(',')
     if @score.save
+      @post.save_tag(arrange_list)
       flash[:notice] = "楽譜を投稿しました"
       redirect_to score_path(@score.id)
     else
@@ -23,6 +26,26 @@ class Public::ScoresController < ApplicationController
       render :index
     end
 
+  end
+  
+  def save_arrange(sent_arrranges)
+  # アレンジが存在していれば、アレンジの名前を配列として全て取得
+    current_arranges = self.arranges.pluck(:name) unless self.arranges.nil?
+    # 現在取得したアレンジから送られてきたアレンジを除いてoldarrangeとする
+    old_arranges = current_arranges - sent_arranges
+    # 送信されてきたアレンジから現在存在するアレンジを除いたアレンジをnewとする
+    new_arranges = sent_arranges - current_arranges
+
+     # 古いアレンジを消す
+    old_arranges.each do |old|
+      self.arranges.delete　Arrange.find_by(name: old)
+    end
+
+    # 新しいアレンジを保存
+    new_arranges.each do |new|
+      new_arrange_tag = Arrange.find_or_create_by(name: new)
+      self.arranges << new_arrange_tag
+    end
   end
 
   def edit
@@ -35,6 +58,8 @@ class Public::ScoresController < ApplicationController
     @review = Review.new
     @reviews = @score.reviews
     #レビュー全件表示
+    @site = @score.site
+    @arranges = @score.arranges
   end
 
   private
