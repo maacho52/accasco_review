4b1cdd05b4f6fce9af088dc7483b8bd8dfd42e8a
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-06 11:56:31 +0000

    [Add]アレンジ内容の中間モデルとのリレーション

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,6 +3,10 @@ class Score < ApplicationRecord
   has_many :arranges
   belongs_to :user
   belongs_to :site
+  
+    #arrange_tagのリレーション記載
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
 
   enum member: { three: 0, four: 1, five: 2, six: 3, seven_or_more: 4, boys: 5, girls: 6 }
   enum difficulty: { introduction: 0, biginner: 1, intermediate: 2, advanced: 3, superlative: 4 }
