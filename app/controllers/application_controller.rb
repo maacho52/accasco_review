24ea4ff4bbc26e962470bec6f8de9a9149128012
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-09 07:01:31 +0000

    [Create]def configure_permitted_parameters サインイン、アウト後の画面遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource_or_scop)
+    if resource_or_scop.is_a?(Admin)
+
+       admin_root_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scop)
+    if resource_or_scop == :admin
+      admin_session_path
+    else root_path
+
+    end
+  end
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :part, :birthday, :sex, :experience])
+  end
+
 end
