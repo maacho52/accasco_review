1657bc10178cdb97a58b841bd0e11719797747fe
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-09 09:57:42 +0000

    [Create]showアクション user_params

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,11 @@
 class Public::UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :sex, :part, :birthday, :experience, :is_deleted)
+  end
 end
