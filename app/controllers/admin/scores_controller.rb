7c4b7b64177b98defbaf7c01b7060a132a45e30b
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-10 14:42:51 +0000

    [Create]管理者側楽譜投稿一覧・詳細ページ index,show,editアクション score_params

--- app/controllers/admin/scores_controller.rb
+++ app/controllers/admin/scores_controller.rb
@@ -1,2 +1,21 @@
 class Admin::ScoresController < ApplicationController
+  def index
+    @scores = Score.all.page(params[:page]).per(8)
+  end
+  
+  def show
+    @score = Score.find(params[:id])
+    @site = @score.site
+    @score_arranges = @score.arranges
+  end
+  
+  def edit
+    @score = Score.find(params[:id])
+  end
+  
+  private
+  
+  def score_params
+    params.require(:score).permit(:name, :artist, :member, :difficulty, :user_id)
+  end
 end
