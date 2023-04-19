fe893c4b7b222e08d6fe8f0ea1c6a00b7348c222
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-19 12:39:38 +0000

    [Add]showアクション　@comments @score @user

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,2 +1,24 @@
 class Admin::ReviewsController < ApplicationController
+  def index
+    @score = Score.find(params[:score_id])
+    @reviews = Review.all
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @score = Score.find(prams[:score_id])
+    @reveiw = Review.find(params[:id])
+    @comments = @review.comments
+  end
+
+  def edit
+    @user = current_user
+    @reveiw = @user.review
+  end
+
+  private
+
+  def reveiw_params
+    params.require(:review).permit(:user_id, :score_id, :title, :body, :star)
+  end
 end
