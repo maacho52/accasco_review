da278a243826ceca98bf23a555b72134925718a1
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-23 21:51:51 +0000

    [Add]検索タグの設定 accepts_nested_attributes_for

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,13 +4,15 @@ class Score < ApplicationRecord
   belongs_to :user
   belongs_to :site
 
-    #arrange_tagのリレーション記載
+    #arrange_tagのリレーション記載と検索タグ
   has_many :arrange_tags, dependent: :destroy
   has_many :arrange, through: :arrange_tags
+  accepts_nested_attributes_for :arrange_tags, allow_destroy: true
+  accepts_nested_attributes_for :arrange
 
   enum member: { three: 0, four: 1, five: 2, six: 3, seven_or_more: 4, boys: 5, girls: 6 }
   enum difficulty: { introduction: 0, biginner: 1, intermediate: 2, advanced: 3, superlative: 4 }
-  
+
   def save_arrange(sent_arrranges)
   # アレンジが存在していれば、アレンジの名前を配列として全て取得
     current_arranges = self.arranges.pluck(:name) unless self.arranges.nil?
