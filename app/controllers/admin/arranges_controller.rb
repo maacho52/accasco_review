490d6e13e35b05bd48b8efa1f25f95f207ca0867
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-23 09:39:38 +0000

    [Create]編集ページ,編集・アップデート機能

--- app/controllers/admin/arranges_controller.rb
+++ app/controllers/admin/arranges_controller.rb
@@ -1,2 +1,19 @@
 class Admin::ArrangesController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def edit
+    @arrange = Arrange.find(params[:id])
+  end
+  
+  def update
+    @arrange = Arrange.find(params[:id])
+    @arrange.update(arrange_params)
+    redirect_to admin_root_path
+  end
+  
+  private
+  
+  def arrange_params
+    params.require(:arrange).permit(:body)
+  end
 end
--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,2 +1,19 @@
 class Admin::SitesController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def edit
+    @site = Site.find(params[:id])
+  end
+  
+  def update
+    @site = Site.find(params[:id])
+    @site.update(site_params)
+    redirect_to admin_root_path
+  end
+  
+  private
+  
+  def site_params
+    params.require(:site).permit(:name)
+  end
 end
