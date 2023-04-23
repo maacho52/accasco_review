930ba36f0ad9c65af988f461727844b4c48837ef
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-23 09:10:48 +0000

    [Create]topアクション

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,2 +1,32 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
+
+  def top
+    #一覧
+    @sites = Site.all
+    @arranges = Arrange.all
+
+    #サイト新規登録
+    @site_new = Site.new
+    @site = Genre.new(site_params)
+    @site.save
+    redirect_to admin_root_path
+
+    #アレンジ新規登録
+    @arrange_new = Arrange.new
+    @arrange = Arrange.new(arrange_params)
+    @arrange.save
+    redirect_to admin_root_path
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(:name)
+  end
+
+  def arrange_params
+    params.require(:arrange).permit(:body)
+  end
+
 end
