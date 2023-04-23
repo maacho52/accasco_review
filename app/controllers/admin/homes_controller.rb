3436af42f0acd40926a929a55242fdb73b4e7a36
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-23 10:46:39 +0000

    [Delete]createアクション

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -5,18 +5,9 @@ class Admin::HomesController < ApplicationController
     #一覧
     @sites = Site.all
     @arranges = Arrange.all
-
-    #サイト新規登録
+    
     @site_new = Site.new
-    @site = Genre.new(site_params)
-    @site.save
-    redirect_to admin_root_path
-
-    #アレンジ新規登録
-    @arrange_new = Arrange.new
-    @arrange = Arrange.new(arrange_params)
-    @arrange.save
-    redirect_to admin_root_path
+    @arrange_new = Arrange.new    
   end
 
   private
