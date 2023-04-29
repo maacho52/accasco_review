98a7da05fc9399376175499c1fa67874a2c61d47
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-04-29 06:06:10 +0000

    [Add]before_action :authenticate_user

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,18 +1,20 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @user = current_user
     @user.birthday = (Date.today.strftime('%Y%m%d').to_i - @user.birthday.strftime('%Y%m%d').to_i) / 10000
   end
-  
+
   def edit
     @user = current_user
   end
-  
+
   def score_index
     @user = current_user
     @scores = @user.scores.page(params[:page]).per(12)
   end
-  
+
   def review_index
     @user = current_user
     @reviews = @user.reveiws.page(params[:page]).per(12)
