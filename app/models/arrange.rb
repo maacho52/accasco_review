03a6d9e60f65a49c198da713f4d9ca625ec7551a
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-05-06 06:49:18 +0000

    [Add]has_many scoreにvalidate :false

--- app/models/arrange.rb
+++ app/models/arrange.rb
@@ -1,7 +1,7 @@
 class Arrange < ApplicationRecord
   #belongs_to :score
   has_many :arrange_tags, dependent: :destroy, foreign_key: 'arrange_id'
-  has_many :scores, through: :arrange_tags
+  has_many :scores, through: :arrange_tags, validate: false
   # タグは複数の楽譜投稿を持つ　それは、arrange_tagsを通じて参照できる
   validates :body, uniqueness: true, presence: true
 end
