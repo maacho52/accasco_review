af0895b85444b4d181fdb499fd40f82ce978fb38
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-05-10 14:03:37 +0000

    [Fix]リレーション arrange_tags → score_arranges

--- app/models/arrange.rb
+++ app/models/arrange.rb
@@ -1,7 +1,7 @@
 class Arrange < ApplicationRecord
   #belongs_to :score
-  has_many :arrange_tags, dependent: :destroy, foreign_key: 'arrange_id'
-  has_many :scores, through: :arrange_tags, validate: false
-  # タグは複数の楽譜投稿を持つ　それは、arrange_tagsを通じて参照できる
+  has_many :score_arranges, dependent: :destroy, foreign_key: 'arrange_id'
+  has_many :scores, through: :score_arranges, validate: false
+  # アレンジは複数の楽譜投稿を持つ　それは、score_arrangesを通じて参照できる
   validates :body, uniqueness: true, presence: true
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -5,32 +5,19 @@ class Score < ApplicationRecord
 
   has_one_attached :image
 
-    #arrange_tagのリレーション記載
-  has_many :arrange_tags, dependent: :destroy
-  has_many :arranges, through: :arrange_tags
+    #score_arrangesのリレーション記載
+  has_many :score_arranges, dependent: :destroy
+  has_many :arranges, through: :score_arranges
 
   enum member: { three: 0, four: 1, five: 2, six: 3, seven_or_more: 4, boys: 5, girls: 6 }
   enum difficulty: { introduction: 0, biginner: 1, intermediate: 2, advanced: 3, superlative: 4 }
 
-  def save_arrange(sent_arranges)
-  # アレンジが存在していれば、アレンジの名前を配列として全て取得
-    current_arranges = self.arranges.pluck(:body) unless self.arranges.nil?
-    # 現在取得したアレンジから送られてきたアレンジを除いてoldarrangeとする
-    old_arranges = current_arranges - sent_arranges
-    # 送信されてきたアレンジから現在存在するアレンジを除いたアレンジをnewとする
-    new_arranges = sent_arranges - current_arranges
-
-     # 古いアレンジを消す
-    old_arranges.each do |old|
-      self.score_arranges.delete　ScoreArrange.find_by(body: old)
-    end
-
-    # 新しいアレンジを保存
-    new_arranges.each do |new|
-      new_score_arranges = ScoreArrange.find_or_create_by(body: new)
-      self.score_arranges << new_score_arrange
+  def save_arrange(arrange_ids)
+    arrange_ids.each do |arrange_id|
+      ScoreArrange.create(score_id: id, arrange_id: arrange_id)
     end
   end
+
 
   def self.search(search)
     if search != ""
@@ -39,7 +26,7 @@ class Score < ApplicationRecord
       Score.all
     end
   end
-  
+
   def get_image(width, height)
    unless image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
