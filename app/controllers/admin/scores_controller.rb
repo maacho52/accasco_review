812d2c8c1d649ce993c7635232dc43833cb75b9f
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-05-20 01:10:17 +0000

    [Add]updateアクション[Delete]showアクション@score_arranges

--- app/controllers/admin/scores_controller.rb
+++ app/controllers/admin/scores_controller.rb
@@ -6,12 +6,22 @@ class Admin::ScoresController < ApplicationController
   def show
     @score = Score.find(params[:id])
     @site = @score.site
-    @score_arranges = @score.arranges
+    #@score_arranges = @score.arranges
   end
 
   def edit
     @score = Score.find(params[:id])
     @sites = Site.all
+  end
+  
+  def update
+    @score = Score.find(params[:id])
+    @sites = Site.all
+    if @score.update(score_params)
+      redirect_to admin_score_path
+    else
+      render :edit
+    end  
   end
 
   private
