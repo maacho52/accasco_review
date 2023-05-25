3a924a994de91e56d970f1a1d605e172e9be424b
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-05-25 13:43:14 +0000

    [Add]newアクション[Fix]createアクションif文記述

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,17 +1,25 @@
 class Public::CommentsController < ApplicationController
-  
+  before_action :authenticate_user!
+
+  def new
+    @user = current_user
+    @review = Review.find(params[:review_id])
+    @score = @review.score
+    @comment = current_user.comments.new
+  end
+
   def create
     @review = Review.find(params[:review_id])
     @comment = current_user.comments.new(comment_params)
     if @comment.save
-      redirect_back(fallback_location: root_path)  #コメント送信後は、一つ前のページへリダイレクトさせる。
+      redirect_to score_review_path(@review.score, @review)
     else
-      redirect_back(fallback_location: root_path)  #同上
+      render :new
     end
   end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:body, :review_id).merge(user_id: current_user.id)
   end
