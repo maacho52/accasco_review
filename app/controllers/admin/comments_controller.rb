196f5f872a4aa97ddac9d18e3e6a71719d364ea8
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-05-25 13:37:04 +0000

    [Create]destroyアクション パラメーター

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,2 +1,15 @@
 class Admin::CommentsController < ApplicationController
+  def destroy
+    @comment = Comment.find(params[:id])
+    @score = @comment.review.score
+    @review = @comment.review
+    @comment.destroy
+    redirect_to admin_score_review_path(@score, @review)
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :reiview_id)
+  end  
 end
