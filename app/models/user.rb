8acda5631eec8bcb414a6ffbb9ed08af0a7b6b49
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-05-30 14:00:49 +0000

    [Fix]元に戻す

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   #  end
   #end
   has_one_attached :image
-
+  
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -28,10 +28,10 @@ class User < ApplicationRecord
       user.birthday = '2000-01-01'
     end
   end
-
+  
   def get_image(width, height)
    unless image.attached?
-    file_path = Rails.root.join('no_image.jpg')
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
    end
     image.variant(resize_to_limit: [width, height]).processed
