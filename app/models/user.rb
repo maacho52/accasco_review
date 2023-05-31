6609e6c10531a19e16e52caba84da0ef508d7f50
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-05-31 12:12:36 +0000

    [Fix]元に戻す

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,13 @@ class User < ApplicationRecord
   end
 
   def get_image(width, height)
-
+    #binding.pry
+   if !profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    #binding.pry
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   else
     profile_image.variant(resize_to_limit: [width, height]).processed
+   end
   end
 end
