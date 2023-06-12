c29b7bd682223204366a4d5c48ea2e6a06008717
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-06-12 13:26:40 +0000

    [Add]title,bodyのバリデーション

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :score
   validates :star, presence: true
-
+  validates :title, presence: true
+  validates :body, presence: true
   # 星の評価の空を禁止する、且つ1以上、5未満
   validates :star, numericality: {
     less_than_or_equal_to: 5,
